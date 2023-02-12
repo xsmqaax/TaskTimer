65a7632f2aff6b59a45c33b0b89651a8a3f07667
Saad Alsayed <smqaa.github@gmail.com>
2023-02-12 20:55:31 +0300

    app logo for the website, and a new font family

--- src/Components/Welcoming.js
+++ src/Components/Welcoming.js
@@ -3,15 +3,19 @@ import '../Styles/welcoming.css';
 export function Welcoming() {
 
     return (
-        <div className='main-container '>
+        <div className='main-container container-fluid'>
 
-            <div className='divs'>
+            <div className='logo-div container-sm'>
+                <img ></img>
+            </div>
+
+            <div className='divs container-sm'>
                 <h1>أهلاً</h1>
                 <input className='name-input' type='text' placeholder=' أدخل اسمك هنا...'></input>
             </div>
 
-            <div className='divs'>
-                <button type='submit' className='btn btn-outline-info btn-lg start-btn'>ابدأ</button>
+            <div className='divs container-sm'>
+                <button type='submit' className='start-btn'>ابدأ</button>
             </div>
 
         </div>
