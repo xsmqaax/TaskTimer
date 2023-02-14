837f0980f65563ac5f750a88bd59ff69fbdf1e86
Saad Alsayed <smqaa.github@gmail.com>
2023-02-14 19:17:30 +0300

    done with the styling of TaskBoard.js

--- src/Components/TaskBoard.js
+++ src/Components/TaskBoard.js
@@ -1,23 +1,29 @@
 import '../Styles/taskBoard.css';
+import { TimerCard } from './TimerCard';
+import { DetailCard } from './DetailCard';
 
 export function TaskBoard(){
-    
+    const taskName = "إسم المهمة هنا"
 
     return(
-        <div className='board-container shadow p-3 mb-5 bg-body-tertiary rounded'>
+        <div className='board-container shadow p-3 mb-5 bg-body-tertiary rounded main'>
             <div className='top-row row'>
-                <div className='col'>#3<img src='./Images/tasks-icon.png'></img></div>
+                <div className='col'> {taskName} </div>
             </div>
             <div className='main-row row'>
 
-                <div className='right-col col-12 col-md-3'> #4  </div>
-                <div className='left-col col-12 col-md-9'> #5 </div>
+                <div className='right-col col-12 col-md-3'> <TimerCard /> </div>
+                <div className='left-col col-12 col-md-9'> <DetailCard /> </div>
             </div>
 
             <div className='main-row row'>
 
-                <div className='right-col col-12 col-md-3'>  </div>
-                <div className='left-col col-12 col-md-9'>   </div>
+                <div className='right-col col-12 col-md-3'> 
+                    <button type="button" className="btn btn-success"> إبدأ</button>
+                 </div>
+                <div className='left-col col-12 col-md-3'>  
+                    <button type="button" className="btn btn-danger"> إيقاف</button>
+                </div>
             </div>
         </div>
     );
